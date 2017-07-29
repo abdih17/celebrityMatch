adc61ae929aee9e88dba5ef53d4e3fd792e43444
Hawa Abdi <abdih17@hotmail.com>
2017-07-29 12:53:36 -0700

    rendering text from picture-display file on app

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import DashboardContainer from '../dashboard-container'
+import PictureDisplay from '../picture-display'
 
 class App extends React.Component{
   render(){
@@ -9,6 +10,7 @@ class App extends React.Component{
       <div>
         <h1> Hello World </h1>
         <DashboardContainer />
+        <PictureDisplay />
       </div>
     )
   }
--- src/component/picture-display/index.js
+++ src/component/picture-display/index.js
@@ -1,1 +1,19 @@
 import React from 'react'
+
+import {
+  pictureCreate,
+  pictureReset,
+} from '../../action/picture-action.js'
+
+class PictureDisplay extends React.Component{
+
+  render(){
+    return (
+      <div>
+        <h3>Pictures</h3>
+      </div>
+    )
+  }
+}
+
+export default PictureDisplay
