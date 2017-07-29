0a62e260ec8f22feae996f4e783372ca54bf03f5
Hawa Abdi <abdih17@hotmail.com>
2017-07-29 12:41:31 -0700

    connected my dashboard component to the app

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import DashboardContainer from '../dashboard-container'
+
 class App extends React.Component{
   render(){
     return (
       <div>
         <h1> Hello World </h1>
+        <DashboardContainer />
       </div>
     )
   }
---
+++ src/component/dashboard-container/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+import {
+  pictureCreate,
+  pictureReset,
+} from '../../action/picture-action.js'
+
+class DashboardContainer extends React.Component{
+  render(){
+    return (
+      <div>
+        <h2>Maryan</h2>
+      </div>
+    )
+  }
+}
+
+export default DashboardContainer
