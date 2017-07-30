3e5e2ab4cacbfc963b06e04c949de854867cf132
Hawa Abdi <abdih17@hotmail.com>
2017-07-30 11:37:02 -0700

    images are reappearing with return statements, need to refactor/revert.

--- src/component/picture-item/index.js
+++ src/component/picture-item/index.js
@@ -9,6 +9,7 @@ let counter = 0
 let rightIndex = 0
 let leftIndex = 0
 let centerIndex = 0
+let currentImages = []
 
 class PictureItem extends React.Component{
   constructor(props){
@@ -21,45 +22,67 @@ class PictureItem extends React.Component{
         'ryanG', 'jason', 'don', 'robert', 'jessie', 'richard',
         'jon', 'orlando', 'liam',
       ],
+      buttonText: 'Generate a Hottie',
       random: false,
-      buttonText: 'Randomly Generate a Celebrity',
+      currentImages: [],
     }
 
     this.renderImage = this.renderImage.bind(this)
     this.getRandomNumber = this.getRandomNumber.bind(this)
+    this.imagesOnDom = this.imagesOnDom.bind(this)
   }
 
   getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-  renderImage() {
-    console.log('hello')
-    leftIndex = parseInt(this.getRandomNumber(0, 20))
+  renderImage(imageName, i) {
+    console.log('(((((((((((((((((((((((((((((((((((((((((hello')
+    leftIndex = parseInt(this.getRandomNumber(1, 20))
     console.log(this.state.imagesArray[leftIndex])
-    centerIndex = parseInt(this.getRandomNumber(0, 20))
+    centerIndex = parseInt(this.getRandomNumber(1, 20))
     console.log(this.state.imagesArray[centerIndex])
-    rightIndex = parseInt(this.getRandomNumber(0, 20))
+    rightIndex = parseInt(this.getRandomNumber(1, 20))
     console.log(this.state.imagesArray[rightIndex])
 
+    if (leftIndex === centerIndex || centerIndex === rightIndex || leftIndex === rightIndex) {
+      this.state.currentImages = [leftIndex, centerIndex, rightIndex]
+      console.log('??????????????????????????', this)
+      console.log('duplicate caught')
+      this.renderImage()
+    } else {
+      counter++
+      this.state.currentImages = [leftIndex, centerIndex, rightIndex]
+      console.log('**************************', this)
+      // console.log('COUNTER*********************************', counter)
+      return this.imagesOnDom(imageName, i)
+    }
+  }
+
+  imagesOnDom(imageName, i) {
+    // console.log('IMAGENAME= ', imageName)
+    // console.log('KEY I++ ', i)
+    // console.log('COUNTER-------------------------------', counter)
     return (
       <div>
-        <ul>
-          <li>
-            <img src={require('../images/' + this.state.imagesArray[leftIndex] + '.png')} />
-          </li>
+        <li>
+          <img src={require('../images/' + this.state.imagesArray[leftIndex] + '.png')}/>
+          {console.log('LEFTINDEX', this.state.imagesArray[leftIndex])}
+        </li>
 
-          <li>
-            <img src={require('../images/' + this.state.imagesArray[centerIndex] + '.png')} />
-          </li>
+        <li>
+          <img src={require('../images/' + this.state.imagesArray[centerIndex] + '.png')}/>
+          {console.log('CENTERINDEX', this.state.imagesArray[centerIndex])}
+        </li>
 
-          <li>
-            <img src={require('../images/' + this.state.imagesArray[rightIndex] + '.png')} />
-          </li>
-        </ul>
+        <li>
+          <img src={require('../images/' + this.state.imagesArray[rightIndex] + '.png')}/>
+          {console.log('RIGHTINDEX', this.state.imagesArray[rightIndex])}
+        </li>
       </div>
     )
   }
+
 
   render() {
     console.log(this)
@@ -67,9 +90,11 @@ class PictureItem extends React.Component{
       <div className="gallery">
         <h5>THESE IS THE IMAGES</h5>
         <div className="images-container">
-          {console.log(this)}
-          <button onClick={this.renderImage}> {this.state.buttonText} </button>
-          
+          <ul>
+            {this.state.imagesArray.map((imageName, i) => {
+              return this.renderImage(imageName, i)
+            })}
+          </ul>
         </div>
       </div>
     )
